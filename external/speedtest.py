d033c1cd277b931052750bf371f717788c5c72f5
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-23 14:24:33 +0000

    Adding tarfile member sanitization to extractall()

--- external/speedtest.py
+++ external/speedtest.py
@@ -63,7 +63,26 @@ def download_and_extract():
             os.chmod(wd.joinpath("CloudflareST").as_posix(), 0o755)
         else:
             with tarfile.open(f.name) as tar:
-                tar.extractall(wd)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, wd)
 
 
 def run_tester():
